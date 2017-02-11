07c02a9025c58cfb3c41c91ab775fda4fa22f8e0
Paul Vergeev <paulvergeev@gmail.com>
2017-02-11 16:50:52 +0300

    Add exception handling
    Also minor code readability changes.

--- movie_budget.py
+++ movie_budget.py
@@ -1,4 +1,5 @@
-from download_db import make_tmdb_api_request
+from download_db import get_movie_info_from_tmdb
+from urllib.error import HTTPError
 from sys import argv
 from sys import exit
 
@@ -7,8 +8,15 @@ if __name__ == '__main__':
         exit('Неверное количество аргументов.')
     movie_id = int(argv[1])
     api_key = argv[2]
-    movie = make_tmdb_api_request('/movie/' + str(movie_id), api_key)
 
-    message = 'Бюджет фильма \"' + movie['title'] +\
-              '\" составляет ' + str(movie['budget']) + ' долларов.'
+    try:
+        movie = get_movie_info_from_tmdb(movie_id, api_key)
+    except HTTPError as error:
+        if error.code == 401:
+            exit('Неверный API ключ.')
+        else:
+            exit('Ошибка %s.' % error.code)
+
+    message = 'Бюджет фильма \"%s\" составляет %d долларов.'
+    message = message % (movie['title'], movie['budget'])
     print(message)
