ed88308a67b917e4aba7f726687f3461f02f180e
Pavel Vergeev <patrnk@gmail.com>
2017-02-13 11:23:36 +0300

    Rename and document get_rating

--- recommend.py
+++ recommend.py
@@ -11,42 +11,49 @@ def count_common_elements(lhs, rhs, key):
 
 
 def almost_equal(lhs, rhs, error_range):
-    if lhs is None or rhs is None:
+    if lhs or rhs is None:
         return False
-    return lhs - error_range <= rhs and rhs <= lhs + error_range
+    return lhs - error_range <= rhs <= lhs + error_range
 
 
-def get_rating(candidate, model):
+def get_similarity_rating(movie1, movie2):
+    '''Returns an integer indicating how similar the two movies.
+        
+    Minimum possible rating is 0. 
+    Maximum possible rating is determined by get_similarity_rating(a, a).
+    Note that get_similarity_rating(a, b) is not necessarily 
+    equal to get_similarity_rating(b, a).
+    '''
     weight = {'belongs_to_collection': 1000, 'keywords': 200, 'genres': 100,
               'production_companies': 150, 'budget': 30, 'runtime': 40, 
               'vote_average': 30}
     rating = 0
 
-    candidate_collection = candidate['belongs_to_collection']
-    model_collection = model['belongs_to_collection']
-    if candidate_collection is not None and model_collection is not None:
-        if candidate_collection['id'] == model_collection['id']:
+    movie1_collection = movie1['belongs_to_collection']
+    movie2_collection = movie2['belongs_to_collection']
+    if movie1_collection is not None and movie2_collection is not None:
+        if movie1_collection['id'] == movie2_collection['id']:
             rating += weight['belongs_to_collection']
 
     strict_criteria = ['keywords', 'genres', 'production_companies']
     for criterion in strict_criteria:
-        common_elements = count_common_elements(candidate, model, criterion)
+        common_elements = count_common_elements(movie1, movie2, criterion)
         rating += common_elements * weight[criterion]
 
     loose_criteria = ['budget', 'runtime', 'vote_average']
     accuracy = {'budget': 0.2, 'runtime': 0.15, 'vote_average': 0.2}
     for criterion in loose_criteria:
-        is_equal = almost_equal(candidate[criterion], model[criterion],
-                                model[criterion] * accuracy[criterion])
+        is_equal = almost_equal(movie1[criterion], movie2[criterion],
+                                movie2[criterion] * accuracy[criterion])
         rating += int(is_equal) * weight[criterion]
         
     return rating
 
 
-def load_movies(filename):
-    if not exists(filename):
+def load_movies_from_file(filepath):
+    if not exists(filepath):
         return None
-    with open(filename, 'r') as f:
+    with open(filepath, 'r') as f:
         return load(f)
 
 
@@ -57,7 +64,7 @@ if __name__ == '__main__':
     top_count = int(argv[2])
     filename = argv[3]
 
-    database = load_movies(filename)
+    database = load_movies_from_file(filename)
     if database is None:
         exit('Файл не найден.')
 
@@ -65,7 +72,7 @@ if __name__ == '__main__':
         exit('Этого фильма нет в базе.')
     target = database.pop(query)
 
-    chart = [(get_rating(movie, target), title) 
+    chart = [(get_similarity_rating(movie, target), title) 
              for title, movie in database.items()]
     chart.sort(reverse = True)
     
