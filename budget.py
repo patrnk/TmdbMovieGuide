777f1590eff85a1218e41555178b61e6d9f77b60
Paul Vergeev <patrnk@gmail.com>
2017-02-16 10:57:25 +0300

    Get rid of get_movie()
    The method turned out to be superfluous.

--- budget.py
+++ budget.py
@@ -13,11 +13,6 @@ def get_argument_parser():
     return parser
 
 
-def get_movie(movie_id, api_key):
-    movie = get_movie_info_from_tmdb(movie_id, api_key)
-    return movie
-
-
 def print_movie_budget(movie):
     message = 'The budget of the movie \"%s\" is %s.'
     budget = "$%d" % movie['budget'] if movie['budget'] != 0 else 'unknown'
@@ -34,5 +29,5 @@ if __name__ == '__main__':
     if not is_valid_tmdb_api_v3_key(api_key):
         exit('Bad API key.')
 
-    movie = get_movie(args.movie_id, api_key)
+    movie = get_movie_info_from_tmdb(args.movie_id, api_key)
     print_movie_budget(movie)
